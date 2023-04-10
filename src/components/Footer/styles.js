ab0edbe1654e1d8832e2bdbecfa7011f4ae815b6
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-10 15:35:33 -0300

    feat(Footer): implement footer

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: 2rem;
+  box-shadow: 0px -10px 20px rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0rem 2rem;
+  position: fixed;
+  bottom: 0;
+  background-color: aliceblue;
+`;

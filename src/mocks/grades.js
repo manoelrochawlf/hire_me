17e63dd9fc00c8f1693d90009f1035a2649ea860
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-11 14:07:02 -0300

    chore: adding grades data

---
+++ src/mocks/grades.js
@@ -0,0 +1,8 @@
+export const gradesData = [
+    { module: "Módulo 1", grade: 10.0 },
+    { module: "Módulo 2", grade: 9.7 },
+    { module: "Módulo 3", grade: 10.0 },
+    { module: "Módulo 4", grade: 9.1 },
+    { module: "Módulo 5", grade: 9.6 },
+    { module: "Média final", grade: 9.7 },
+  ];

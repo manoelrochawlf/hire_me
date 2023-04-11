73b6ed7977b14cd8092469e79f9b6be5ee0de094
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-11 10:36:21 -0300

    chore: add link of router-dom

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { HeaderContainer, Contact, Item, Items, Logo, MobContainer, DropdownContainer, DropdownItem } from "./styles";
 import pizza from "../../assets/icons/pizza.png"
+import { Link } from "react-router-dom";
 
 console.log(pizza);
 
@@ -15,21 +16,37 @@ const Header = () => {
 
     return ( 
         <HeaderContainer>
-            <Logo>hire.me</Logo>
+            <Link to="/">
+              <Logo>hire.me</Logo>
+            </Link>
             <Items>
+              <Link to="/about">
                 <Item>sobre mim</Item>
+              </Link>
+              <Link to="/skills">
                 <Item>skills</Item>
+              </Link>
+              <Link to="feedbacks">
                 <Item>feedbacks</Item>
+              </Link>
             </Items>
            <MobContainer onClick={handleDropdownToggle} className={pizzaClassName}>
-            <img src={pizza} alt="sdas" />
+            <img src={pizza} alt="pizza" />
            </MobContainer>
            {dropdownOpen && 
         <DropdownContainer>
+          <Link to="/about">
           <DropdownItem>sobre mim</DropdownItem>
+          </Link>
+          <Link to="/skills">
           <DropdownItem>skills</DropdownItem>
+          </Link>
+          <Link to="/feedbacks">
           <DropdownItem>feedbacks</DropdownItem>
+          </Link>
+          <Link>
           <DropdownItem>fale comigo :)</DropdownItem>
+          </Link>   
         </DropdownContainer>
       }
            <Contact>fale comigo :)</Contact>

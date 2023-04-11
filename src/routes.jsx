70498ec3f612b62dbc406cdb22b3890cf830f041
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-11 15:41:52 -0300

    chore: add talktome route

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import AboutMe from './pages/AboutMe';
 import Skills from './pages/Skills';
 import Achievements from './pages/Achievements';
+import TalkToMe from './pages/TalkToMe';
 
 const Router = () => {
     return (
@@ -12,6 +13,7 @@ const Router = () => {
             <Route path="/about" element={<AboutMe />} />
             <Route path="/skills" element={<Skills />} />
             <Route path="/achievements" element={<Achievements />} />
+            <Route path="/talktome" element={<TalkToMe />} />
         </Routes>
     );
 }

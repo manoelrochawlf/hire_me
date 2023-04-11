31e1eb2eb62cab2b6f265887d2c542f75a9b450c
Manoel Rocha <manoelrocha399@gmail.com>
2023-04-11 10:33:25 -0300

    feat: add mocking datas

---
+++ src/mocks/tools.js
@@ -0,0 +1,66 @@
+import nest from "../assets/icons/nest.png"
+import javascript from"../assets/icons/javascript.png"
+import typescript from"../assets/icons/typescript.png"
+import css from"../assets/icons/css.png"
+import html from"../assets/icons/html.png"
+import mongodb from"../assets/icons/mongodb.png"
+import next from"../assets/icons/next.png"
+import postgressql from"../assets/icons/postgressql.png"
+import prisma from"../assets/icons/prisma.png"
+import express from"../assets/icons/express.png"
+import node from"../assets/icons/node.png"
+import react from"../assets/icons/react.png"
+
+export const frontTools = [
+	{
+		name:       'Javascript',
+		icon:       javascript
+	},
+	{
+		name:       'Typescript',
+		icon:       typescript
+	},
+	{
+		name:       'React.js',
+		icon:       react
+	},
+    {
+		name:       'Next',
+		icon:       next
+	},
+	{
+		name:       'HTML5',
+		icon:       html
+	},
+    {
+		name:       'CSS3',
+		icon:       css
+	}
+]
+
+export const backTools = [
+	{
+		name:       'Node',
+		icon:       node
+	},
+    {
+		name:       'Express',
+		icon:       express
+	},
+	{
+		name:       'Nest',
+		icon:       nest
+	},
+    {
+		name:       'MongoDB',
+		icon:       mongodb
+	},
+    {
+		name:       'PostgresSQL',
+		icon:       postgressql
+	},
+    {
+		name:       'Prisma',
+		icon:       prisma
+	}
+]
